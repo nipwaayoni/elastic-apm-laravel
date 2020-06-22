c2fd64a10d2641887a4649d9cbe183929867de27
Dirk Tepe <dstepe@gmail.com>
2020-06-22 18:12:08 -0400

    Use Route::current() to get actual Laravel route URI when specified in config

--- tests/TestCase.php
+++ tests/TestCase.php
@@ -3,18 +3,11 @@
 
 namespace Nipwaayoni\ElasticApmLaravel\Tests;
 
-use Nipwaayoni\ElasticApmLaravel\Apm\SpanCollection;
 use Nipwaayoni\ElasticApmLaravel\Facades\ElasticApm;
 use Nipwaayoni\ElasticApmLaravel\Providers\ElasticApmServiceProvider;
-use Nipwaayoni\Helper\Timer;
 
 class TestCase extends \Orchestra\Testbench\TestCase
 {
-    /** @var SpanCollection  */
-    protected $spans;
-    /** @var Timer  */
-    protected $timer;
-
     protected function getPackageProviders($app)
     {
         return [ElasticApmServiceProvider::class];
@@ -25,25 +18,5 @@ class TestCase extends \Orchestra\Testbench\TestCase
         return [
             'ElasticApm' => ElasticApm::class
         ];
-    }
-
-    protected function assertSpanAddedWithName(string $name, int $index = 0): void
-    {
-        $this->assertTrue($this->spans->isNotEmpty());
-
-        $spanData = $this->spans[$index];
-
-        $this->assertArrayHasKey('name', $spanData);
-        $this->assertEquals($name, $spanData['name']);
-    }
-
-    protected function assertSpanAddedWithType(string $type, int $index = 0): void
-    {
-        $this->assertTrue($this->spans->isNotEmpty());
-
-        $spanData = $this->spans[$index];
-
-        $this->assertArrayHasKey('type', $spanData);
-        $this->assertEquals($type, $spanData['type']);
     }
 }
